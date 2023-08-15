c31cd681d7579da5bd42c33803c0c1999d42a09b
Simon Toledano <139832522+simontoledano@users.noreply.github.com>
2023-08-15 04:34:25 +0000

    lat and lng obtained

--- umbrella.rb
+++ umbrella.rb
@@ -2,14 +2,27 @@ require "http"
 
 pp "Where are you located?"
 
-#user_location = gets.chomp 
-
-user_location = "Boston"
+user_location = gets.chomp.gsub(" ", "%20")
 
 pirate_weather_api_key =ENV.fetch("PIRATE_WEATHER_KEY")
 
 maps_url="https://maps.googleapis.com/maps/api/geocode/json?address=" + user_location + "&key=" + ENV.fetch("GMAPS_KEY")
 
+require "http"
+#this line of code I am a bit unsure of
 resp = HTTP.get(maps_url)
 
-pp resp
+raw_response = resp.to_s
+
+require "json"
+
+parsed_response = JSON.parse(raw_response)
+
+results_fetch = parsed_response.fetch("results")
+
+#below is a series of digs to get the longitude and latitude 
+first_dig = results_fetch.at(0)
+second_dig = first_dig.fetch("geometry")
+third_dig = second_dig.fetch("location")
+latitude = third_dig.fetch("lat")
+longitude = third_dig.fetch("lng")
