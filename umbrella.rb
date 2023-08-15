7fa7766923bede0e1919a9162cef06c91bca4a9a
Simon Toledano <139832522+simontoledano@users.noreply.github.com>
2023-08-15 03:25:43 +0000

    Obtaining Pirate Weather for Location

--- umbrella.rb
+++ umbrella.rb
@@ -1,1 +1,11 @@
-pp "Simon"
+require http
+
+pp "Where are you located?"
+
+#user_location = gets.chomp 
+
+user_location = "Boston"
+
+pirate_weather_api_key = ENV.fetch("PIRATE_WEATHER_KEY")
+
+pirate_weather_url= "https://maps.googleapis.com/maps/api/geocode/json?address=" + user_location + "&key=" + ENV.fetch("GMAPS_KEY")
