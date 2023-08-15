91fdee5709bed9cb9cd5282ddd3867c9b725a095
Simon Toledano <139832522+simontoledano@users.noreply.github.com>
2023-08-15 04:02:17 +0000

    Updated Resp

--- Gemfile
+++ Gemfile
@@ -1,3 +1,8 @@
+# /Gemfile
+
 source "https://rubygems.org"
 
 gem "http"
+gem "activesupport"
+gem "awesome_print"
+gem "pry-byebug"
--- umbrella.rb
+++ umbrella.rb
@@ -1,4 +1,4 @@
-require http
+require "http"
 
 pp "Where are you located?"
 
@@ -6,11 +6,9 @@ pp "Where are you located?"
 
 user_location = "Boston"
 
-pirate_weather_api_key = ENV.fetch("PIRATE_WEATHER_KEY")
+pirate_weather_api_key =ENV.fetch("PIRATE_WEATHER_KEY")
 
-pirate_weather_url= "https://maps.googleapis.com/maps/api/geocode/json?address=" + user_location + "&key=" + ENV.fetch("GMAPS_KEY")
-
-require "http"
+maps_url="https://maps.googleapis.com/maps/api/geocode/json?address=" + user_location + "&key=" + ENV.fetch("GMAPS_KEY")
 
 resp = HTTP.get(maps_url)
 
