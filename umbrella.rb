e888a6f76546752b70cfb52c361d3989d1e027e7
Simon Toledano <139832522+simontoledano@users.noreply.github.com>
2023-08-15 03:47:54 +0000

    Change

--- umbrella.rb
+++ umbrella.rb
@@ -9,3 +9,9 @@ user_location = "Boston"
 pirate_weather_api_key = ENV.fetch("PIRATE_WEATHER_KEY")
 
 pirate_weather_url= "https://maps.googleapis.com/maps/api/geocode/json?address=" + user_location + "&key=" + ENV.fetch("GMAPS_KEY")
+
+require "http"
+
+resp = HTTP.get(maps_url)
+
+pp resp
